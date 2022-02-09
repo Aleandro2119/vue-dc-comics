b8486c20de43c740b41692eb4308116c711b7e77
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-09 16:01:27 +0100

    Add import HeaderBottom

---
+++ src/components/HeaderBottom.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <div class="container">
+      <p>--Content goes here--</p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderBottom",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: #1c1c1c;
+  padding: 10px;
+  height: 100px;
+
+  p {
+    font-size: 24px;
+    color: white;
+  }
+}
+</style>
